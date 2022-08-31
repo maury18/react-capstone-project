494db8ea7934dbbe86440ebebd3c8cae0e4b009a
Mauricio <tryptic-st@outlook.com>
2022-08-31 10:11:48 -0500

    Add components

---
+++ src/Redux/Components/navBar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => (
+    <nav className="navbar">
+      <ul className="navbar-searchbar">
+        <li><h2 className="navbar-heading">Search your nintendo character</h2></li>
+      </ul>
+    </nav>
+  );
+  
+  export default Navbar;
+  
